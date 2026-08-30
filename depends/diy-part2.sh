8ae5f783cba1c17404618dff5ebac05bafb69477
pcy0124m <765266885@qq.com>
2026-08-30 15:44:08 +0800

    fix: 修复 RE-SP-01B sysupgrade 网络不通 - 恢复 gmac0+gmac1 完整网络配置

--- depends/diy-part2.sh
+++ depends/diy-part2.sh
@@ -69,10 +69,13 @@ define Device/jdcloud_re-sp-01b
   IMAGE_SIZE := 27328k
   DEVICE_VENDOR := JDCloud
   DEVICE_MODEL := RE-SP-01B
-  DEVICE_PACKAGES := kmod-mt7603 kmod-mt7615-firmware \
-    kmod-mmc-mtk kmod-usb3
-endef
-TARGET_DEVICES += jdcloud_re-sp-01b
+   DEVICE_PACKAGES := kmod-mt7603 kmod-mt7615-firmware \
+     kmod-mmc-mtk kmod-usb3
+   NETWORKING := wan:lan
+   LAN_PORTS := 0 1
+   WAN_PORT := 5
+ endef
+ TARGET_DEVICES += jdcloud_re-sp-01b
 
 MKEOF
     echo "已添加 RE-SP-01B 设备定义到 mt7621.mk"
@@ -99,6 +102,11 @@ if [ -d "target/linux/ramips/dts" ]; then
 		led-failsafe = &led_status_red;
 		led-running = &led_status_green;
 		led-upgrade = &led_status_blue;
+		home = &led_status_green;
+		ethernet0 = &gmac0;
+		ethernet1 = &gmac1;
+		wlan0 = &pcie0_wifi0;
+		wlan1 = &pcie1_wifi0;
 	};
 
 	chosen {
@@ -208,28 +216,18 @@ if [ -d "target/linux/ramips/dts" ]; then
 	};
 };
 
+&gmac0 {
+	status = "okay";
+	label = "lan0";
+};
+
 &gmac1 {
 	status = "okay";
 	label = "wan";
-	phy-handle = <&ethphy0>;
-};
-
-&ethphy0 {
-	/delete-property/ interrupts;
 };
 
 &switch0 {
-	ports {
-		port@1 {
-			status = "okay";
-			label = "lan1";
-		};
-
-		port@2 {
-			status = "okay";
-			label = "lan2";
-		};
-	};
+	status = "okay";
 };
 
 &pcie {
@@ -242,6 +240,7 @@ if [ -d "target/linux/ramips/dts" ]; then
 		reg = <0x0000 0 0 0 0>;
 		nvmem-cells = <&eeprom_factory_0>;
 		nvmem-cell-names = "eeprom";
+		mediatek,mtd-eeprom = <&eeprom_factory_0>;
 	};
 };
 
@@ -251,6 +250,7 @@ if [ -d "target/linux/ramips/dts" ]; then
 		reg = <0x0000 0 0 0 0>;
 		nvmem-cells = <&eeprom_factory_8000>;
 		nvmem-cell-names = "eeprom";
+		mediatek,mtd-eeprom = <&eeprom_factory_8000>;
 		ieee80211-freq-limit = <5000000 6000000>;
 	};
 };
